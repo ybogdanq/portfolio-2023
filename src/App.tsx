0c67f91c730fbca3077a1450da87a64b8807e251
Bohdan Yereshchenko <y.bogdan.work@gmail.com>
2023-08-17 19:06:38 +0200

    little features

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { useInView } from 'react-intersection-observer'
 import { AboutMe } from './components/AboutMe'
 import { DeveloperInfo } from './components/DeveloperInfo'
 import { Experience } from './components/Experience'
 import { Footer } from './components/Footer'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
+import cn from "classnames"
 
 function App() {
+	const { inView, ref } = useInView({
+		triggerOnce: true,
+		threshold: 0
+	})
 	return (
 		<>
-			<div className="mx-auto px-4 sm:px-6 pt-7 sm:pt-17 md:pt-20 lg:pt-24 container flex flex-col">
+			<div
+				ref={ref}
+				className={cn(
+					'mx-auto px-4 sm:px-6 pt-7 sm:pt-17 md:pt-20 lg:pt-24 container flex flex-col opacity-0 transition-all duration-1000',
+					{
+						'opacity-100': inView
+					}
+				)}
+			>
 				<div className="flex-1">
 					<DeveloperInfo />
 					<AboutMe />
@@ -17,7 +31,7 @@ function App() {
 					<Projects />
 				</div>
 			</div>
-				<Footer />
+			<Footer />
 		</>
 	)
 }
